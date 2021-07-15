7f267810ff867d7f832d037a1f61d5f19450069d
Jinay Parekh <jinayparekh@cintamedtech>
2021-07-15 21:06:44 +0530

    Cleaning up the response by adding more details

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,24 @@ const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id")
     .exec()
     .then((docs) => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://" + req.get("host") + "/products/" + doc._id,
+            },
+          };
+        }),
+      };
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
@@ -28,8 +42,16 @@ router.post("/", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: result,
+        message: "Created Product Successfully!",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://" + req.get("host") + "/products/" + result._id,
+          },
+        },
       });
     })
     .catch((err) => {
@@ -41,11 +63,18 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then((doc) => {
       console.log("From the DB", doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            url: "http://" + req.get("host") + "/products/" + doc._id,
+          },
+        });
       } else {
         res
           .status(404)
@@ -67,8 +96,13 @@ router.patch("/:productId", (req, res, next) => {
   Product.updateOne({ _id: id }, { $set: updateOps })
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product Updated",
+        request: {
+          type: "GET",
+          url: "http://" + req.get("host") + "/products/" + id,
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -81,7 +115,9 @@ router.delete("/:productId", (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then((result) => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product Deleted",
+      });
     })
     .catch((err) => {
       console.log(err);
